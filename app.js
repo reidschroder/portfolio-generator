d14c0ba9e30a2fdf5abe9abc0f39ce6a25ffb696
Reid Schroder <reidschroder2@gmail.com>
2022-02-04 16:25:38 -0500

    added capture input with node

---
+++ app.js
@@ -0,0 +1,17 @@
+const profileDataArgs = process.argv.slice(2,process.argv.length);
+
+console.log(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    //this..
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }  
+
+    console.log('================');
+
+    //is same as this 
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
