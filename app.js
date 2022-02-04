9f29a5fd47dca3f7362471a8904692f5dd201678
Reid Schroder <reidschroder2@gmail.com>
2022-02-04 18:50:03 -0500

    added intitial html to node files

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const profileDataArgs = process.argv.slice(2,process.argv.length);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
 
-console.log(profileDataArgs);
+const generatePage = (name, github) => {
+    return `
+    <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-const printProfileData = profileDataArr => {
-    //this..
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }  
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">github</a></h2>
+    </body>
+    </html>
+    `;
+} ;
 
-    console.log('================');
-
-    //is same as this 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+console.log(name, github);
+console.log(generatePage(name, github)); 
